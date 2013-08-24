1066bd42157846514b2b16d0790176f0f6a39c1e
MarioStoilov <stoilov_mario@yahoo.com>
2013-08-24 16:37:17 +0300

    Added async homework

---
+++ AsyncWorkers/AsyncWorkers/bin/Debug/AppX/js/default.js
@@ -0,0 +1,51 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232509
+(function () {
+    "use strict";
+
+    WinJS.Binding.optimizeBindingReferences = true;
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+    
+    var primesCalculator;
+
+    app.onactivated = function (args)
+    {
+        
+        primesCalculator = new PrimesCalculator();
+        var calculatePrimesButton = document.getElementById("calculate-primes");
+        var primesFirstInput = document.getElementById("primes-first");
+        var primesLastInput = document.getElementById("primes-last");
+        var primesCountInput = document.getElementById("primes-count");
+
+        calculatePrimesButton.addEventListener("click", function ()
+        {
+            primesCalculator.calculate(primesFirstInput.value, primesLastInput.value, primesCountInput.value).then(function (primes)
+            {
+                var mainContentElement = document.getElementById("main-content-id");
+
+                var innerHTML = "<br/> Calculator returned: ";
+
+
+                for (var i = 0; i < primes.length; i++)
+                {
+                    innerHTML += primes[i] + " ";
+                }
+
+                mainContentElement.innerHTML += innerHTML;
+            },
+            function (error)
+            {
+                var mainContentElement = document.getElementById("main-content-id");
+
+                
+                var innerHTML = "<br/> Calculator returned: " + error;
+                mainContentElement.innerHTML += innerHTML;
+
+            });
+        });
+    };
+
+    app.start();
+})();
---
+++ AsyncWorkers/AsyncWorkers/js/primesCalculator.js
@@ -0,0 +1,55 @@
+﻿/// <reference path="//Microsoft.WinJS.1.0/js/base.js" />
+
+var PrimesCalculator = WinJS.Class.define(function ()
+{
+    this._primesWorkers = new Array();
+    this._freeWorkers = new Array();
+
+    this._primesWorkers[0] = new Worker("/js/primesWorker.js");
+    this._freeWorkers[0] = true;
+
+    this._primesWorkers[1] = new Worker("/js/primesWorker.js");
+    this._freeWorkers[1] = true;
+
+    this._primesWorkers[2] = new Worker("/js/primesWorker.js");
+    this._freeWorkers[2] = true;
+},
+{
+    calculate: function(firstNumber, secondNumber, numberOfPrimes)
+    {
+        var freeworkerIndex = -1;
+        for (var i = 0; i <= 2; i++)
+        {
+            if (this._freeWorkers[i]==true)
+            {
+                freeworkerIndex = i;
+                break;
+            }
+        }
+
+        if (freeworkerIndex == -1)
+        {
+            return new WinJS.Promise(function (complete, error)
+            {
+                error("There is no free worker available now");
+            });
+        }
+        else
+        {
+            var self = this;
+            return new WinJS.Promise(function (complete)
+            {
+                var primes = {};
+                self._primesWorkers[freeworkerIndex].onmessage = function (event)
+                {
+                    self._freeWorkers[freeworkerIndex] = true;
+                    self._primesWorkers[freeworkerIndex].onmessage = null;
+                    primes = event.data;
+                    complete(primes);
+                }
+                self._freeWorkers[freeworkerIndex] = false;
+                self._primesWorkers[freeworkerIndex].postMessage({ firstNumber: firstNumber, lastNumber: secondNumber, numberOfPrimes: numberOfPrimes });
+            });
+        }
+    }
+});
